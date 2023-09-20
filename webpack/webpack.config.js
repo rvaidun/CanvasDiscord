c6be7ad365fe4a0b40c37c62ef8a8e70f77d21cf
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-09-19 17:22:44 -0700

    fixed issues

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -23,9 +23,7 @@ module.exports = {
   },
   plugins: [
     new CopyPlugin({
-      patterns: [
-        { from: ".", to: ".", context: "public", noErrorOnMissing: true },
-      ],
+      patterns: [{ from: ".", to: ".", context: "public" }],
     }),
   ],
 };
